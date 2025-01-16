fb74279d006dd8aa0168e45bea7c2ac3f191879e
Arcee69 <olawalearcee@gmail.com>
2025-01-16 17:24:25 +0100

    Hot Fix

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -11,6 +11,8 @@ import Corporate from "../pages/Corporate";
 import Help from "../pages/Help";
 import Private from "../pages/Private";
 import Resources from "../pages/Resources";
+import PrivacyPolicy from "../pages/Privacy";
+import Terms from "../pages/Terms";
 
 export default function Routers() {
   return (
@@ -27,6 +29,8 @@ export default function Routers() {
           <Route path="/help-and-support" element={<Help />} />
           <Route path="/private" element={<Private />} />
           <Route path="/resources" element={<Resources />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<Terms />} />
         </Route>
       </Routes>
     </div>
