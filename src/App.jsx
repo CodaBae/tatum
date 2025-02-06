8dadafd304e410d3b457eb1b4632b6cecfda8be7
Ijeoma Shalom Mathew <38937931+CodaBae@users.noreply.github.com>
2025-02-06 18:51:30 +0000

    upodate

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Routers from "./routers";
 import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     AOS.init({
       once: true,
     });
+  }); // Runs once when the component mounts
 
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsLoading(false)
-      }, 6000)
-      return () => clearTimeout(timer)
-    }, [])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []); // Runs once when the component mounts
 
   return (
     <div className="app-container">
@@ -34,24 +37,21 @@ function App() {
               <span>Succeeding</span>
               <span>Excelling</span>
               <span>Advancing</span>
-              <span>Prospering</span> 
+              <span>Prospering</span>
               <span className="emphasis">Smiling.</span>
             </div>
           </div>
         </div>
       )}
-       <div className="unveil-container">
+
+      <div className="unveil-container">
         <div className="unveil-section unveil-top-left"></div>
         <div className="unveil-section unveil-top-right"></div>
         <div className="unveil-section unveil-bottom-left"></div>
         <div className="unveil-section unveil-bottom-right"></div>
       </div>
 
-      {!isLoading && (
-           <Routers />
-      )}
-
-     
+      {!isLoading && <Routers />}
     </div>
   );
 }
