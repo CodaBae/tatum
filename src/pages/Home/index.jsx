c099be6329288778246784211e07413d4a8eca48
Shalom Mathew <shalom.mat40@gmail.com>
2025-02-11 01:08:25 +0000

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -355,7 +355,7 @@ const Home = () => {
                   activeIndex === 0
                     ? "animate__animated animate__fadeInRight "
                     : ""
-                } relative  md:right-14  bottom-0 lm:right-28  lm:h-[850px] md:h-[28px] relative lg:right-[40%] lg:top-[0%]  lm:h-[60vh]`}
+                } relative  md:right-14  bottom-0 lm:right-24   md:h-[28px] relative lg:right-[35%] lg:top-[0%]  lm:h-[55vh]`}
               />
             </div>
           </div>
